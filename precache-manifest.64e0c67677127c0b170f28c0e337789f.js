80d6cf5ebdfd41eb0defa99bb81b446ea3c8e411
junaidNadeem <junaid.pk48@gmail.com>
2018-12-13 15:45:33 +0500

    Updates

---
+++ precache-manifest.64e0c67677127c0b170f28c0e337789f.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = [
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/To-Do/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "8ae5a849b9201548c834",
+    "url": "/To-Do/static/css/main.70d19a64.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/To-Do/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "ea4cd936150ada0de742",
+    "url": "/To-Do/static/js/1.ea4cd936.chunk.js"
+  },
+  {
+    "revision": "c6eb9e8e5d91ab06ae05",
+    "url": "/To-Do/static/js/runtime~main.c6eb9e8e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/To-Do/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/To-Do/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/To-Do/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/To-Do/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/To-Do/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/To-Do/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/To-Do/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/To-Do/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/To-Do/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "8ae5a849b9201548c834",
+    "url": "/To-Do/static/js/main.8ae5a849.chunk.js"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/To-Do/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/To-Do/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/To-Do/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/To-Do/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/To-Do/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/To-Do/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/To-Do/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/To-Do/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/To-Do/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/To-Do/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/To-Do/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "ea4cd936150ada0de742",
+    "url": "/To-Do/static/css/1.bcee97ee.chunk.css"
+  },
+  {
+    "revision": "afd059220d53fb91254db5a7cc39f26f",
+    "url": "/To-Do/index.html"
+  }
+];
